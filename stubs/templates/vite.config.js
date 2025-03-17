1999f68db5590074da9d4a17038460f06b9606ec
Iskender TOTOGLU <totoglu@gmail.com>
2025-03-17 07:47:45 +0300

    rename resources folder

--- stubs/templates/vite.config.js
+++ stubs/templates/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
+import { watch } from 'less';
 
 export default defineConfig({
     root: __dirname,
